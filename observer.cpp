a89c3f30854fe425a2c03e64b83ac5932937da67
Diogo Dutra <diogodutra@gmail.com>
2018-12-27 16:16:46 +0000

    Create observer.cpp

---
+++ observer.cpp
@@ -0,0 +1,61 @@
+//Observer Design Pattern applied for Twitter-like app
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Publisher {
+	vector < class AObserver * > subscribers; 
+	string name;
+public:
+	Publisher(string name) {
+		this->name = name;
+		cout << "I am a Publisher and my new name is " << name << '\n';
+	}
+	void attach(AObserver *sub) {
+		subscribers.push_back(sub);
+	}
+	void says(string speech);
+};
+
+class AObserver {
+public:
+	string name;
+	AObserver(string name) {
+		this->name = name;
+		cout << "I am a Subscriber and my new name is " << name << '\n';
+	}
+	void follow(Publisher *pub) {
+		pub->attach(this);
+	}
+	virtual void hears(string speech) = 0;
+};
+
+void Publisher::says(string speech) {
+	cout << name << " says: " << speech << '\n';
+	for (int i = 0; i < subscribers.size(); i++)
+		subscribers[i]->hears(speech);
+}
+
+class Subscriber : public AObserver {
+public:
+	Subscriber(string name) : AObserver(name) {}
+	void hears(string speech) {
+		cout << name << " hears: " << speech << '\n';
+	}
+};
+
+int main() {
+	Publisher speaker("Lecturer");
+	Subscriber sub1("He"), sub2("She");
+	cout << '\n';
+
+	sub1.follow(&speaker);
+	speaker.says("Hi");
+	cout << '\n';
+
+	sub2.follow(&speaker);
+	speaker.says("Hey");
+}
